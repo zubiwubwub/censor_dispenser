bbe868db09423a40bd06b0885d5c1a8478bdc8ba
Ruslan Zubenko <zubenkoruslan@gmail.com>
2020-08-21 11:50:23 +0300

    email_three = split the strings  at blank space and converted into a list

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -23,17 +23,29 @@ proprietary_terms = [
 "self-preservation", "learning algorithm", "her", "Her", "herself", "Herself"
 ]
 
-def censor_two(text, censor_list):
-  for phrase in censor_list:
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
+def censor_list(text, censor_list):
+  for phrase in censor_list: #went through the proprietary_terms list
     censored_letter = ""
-    for i in range(0,len(phrase)):
+    for i in range(0,len(phrase)): #got index of the phrase in the list
       if phrase[i] == " ":
         censored_letter += " "
       else:
         censored_letter += "X"
-    text = text.replace(phrase, censored_letter)
+    text = text.replace(phrase, censored_letter) #  replaced term with censored_letter
   return text
 
-print(censor_two(email_two, proprietary_terms))
+#print(censor_list(email_two, proprietary_terms))
 
 
+def negativity_censor(text, censor_list,  negative_words):
+  text_words = []
+  for i in text.split(" "):
+    list_i = i.split("\n") #split into a list at the new_line
+    for word in list_i:
+      text_words.append(word) # appended words into a text_words list
+  return text_words
+
+
+print(negativity_censor(email_three, proprietary_terms, negative_words))
